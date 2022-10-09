d060c1a56e8fee8e5dcb19d3f7cd898db3127f9a
anantsingh1607 <113285695+anantsingh1607@users.noreply.github.com>
2022-10-10 03:15:03 +0530

    2

---
+++ reversenumbering.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+   for(int a=1;a<=5;a++){
+   int b=5;
+    for(b=5-a+1;b>0;b--){
+cout<<b<<" ";
+
+   }
+   cout<<endl;
+   
+    }
+    return 0;
+}
