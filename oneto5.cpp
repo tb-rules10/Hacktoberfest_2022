d90d4dba650f96c0f908c57322c35df4cccbc102
anantsingh1607 <113285695+anantsingh1607@users.noreply.github.com>
2022-10-10 03:16:41 +0530

    3

---
+++ oneto5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=0;
+int k;
+cout<<"Enter the number till countin2g required";
+cin>>k;
+for (a>=0;a<=k;a++){
+  if(a%2==0) { cout<<a<<endl;
+}}
+
+
+}
